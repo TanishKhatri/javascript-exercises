ebe61417ff202c41758632d59a163106a9e082a0
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-10 15:38:34 +0000

    Improved code after looking at solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,6 @@
-const reverseString = function(str) {
-  let strArray = str.split("");
-  let strArrayLength = strArray.length;
-  let reverseStrArray = [];
-  for (let i=strArrayLength - 1; i >= 0; i--) {
-    reverseStrArray.push(strArray[i]);
-  }
-  let reverseStr = reverseStrArray.join("");
-  return reverseStr;
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
 
-console.log(reverseString("hello world"));
 // Do not edit below this line
 module.exports = reverseString;

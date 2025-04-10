48019f931bb1d978b360c7f17b29cb1ccce0cb9b
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-10 15:00:57 +0000

    Completed 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+let theString = "";
+const repeatString = function(str, num) {
+  for(let i=0; i<num; i++) {
+    theString += str; 
+  }
+  return theString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;

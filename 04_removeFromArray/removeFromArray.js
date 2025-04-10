d6d72c4ba52215675e2d19c8a4a7cc7737c5cbaf
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-10 17:26:24 +0000

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  let index;
+  let i = 0;
+  while(i < itemsToRemove.length) {
+    index = array.indexOf(itemsToRemove[i]);
+    if (index === -1) {
+      i++;
+      continue;
+    }
+    array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line

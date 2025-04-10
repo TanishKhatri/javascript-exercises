11acd109df21e9c4e512b436dbfd998ffef7e1f0
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-10 15:32:03 +0000

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let strArray = str.split("");
+  let strArrayLength = strArray.length;
+  let reverseStrArray = [];
+  for (let i=strArrayLength - 1; i >= 0; i--) {
+    reverseStrArray.push(strArray[i]);
+  }
+  let reverseStr = reverseStrArray.join("");
+  return reverseStr;
 };
 
+console.log(reverseString("hello world"));
 // Do not edit below this line
 module.exports = reverseString;

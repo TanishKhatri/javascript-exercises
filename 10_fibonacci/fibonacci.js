b267368774555dca5981039863501b7138b07da1
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-14 09:52:07 +0000

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  let num = parseInt(number);
+  if (num < 0) {
+    return 'OOPS';
+  }
+  if (num == 0) {
+    return 0;
+  }
+  
+  let fib = [1, 1];
+  for (let i = 2; i <= num - 1; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[fib.length - 1];
 };
 
+console.log(fibonacci(10));
 // Do not edit below this line
 module.exports = fibonacci;

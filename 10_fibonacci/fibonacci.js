d4e5588daa73f1f0415c4d01c32e135912b577c7
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-14 10:31:30 +0000

    Removed unnecessary code and cleaned it up

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,14 +1,11 @@
 const fibonacci = function (number) {
   let num = parseInt(number);
-  if (num < 0) {
-    return 'OOPS';
-  }
-  if (num == 0) {
-    return 0;
-  }
+  if (num < 0) return 'OOPS';
+  if (num == 0) return 0;
   
-  let fib = [1, 1];
-  for (let i = 2; i <= num - 1; i++) {
+  
+  let fib = [0, 1];
+  for (let i = 2; i <= num; i++) {
     fib.push(fib[i - 1] + fib[i - 2]);
   }
   return fib[fib.length - 1];

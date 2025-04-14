2e1ac21c7c638ed541c83e9469ced49a84daf80b
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-14 08:57:37 +0000

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+  palindrome = palindrome.toUpperCase();
+  let palindromeArray = palindrome.split("");
+  let aToZ = /^[A-Za-z0-9]*$/;
+  let palindromeArrayOnlyAToZ = [];
+  for (char of palindromeArray) {
+    if (aToZ.test(char)) {
+      palindromeArrayOnlyAToZ.push(char);
+    }
+  }
+  let reversePalindromeArrayOnlyAToZ = palindromeArrayOnlyAToZ.toReversed();
+  let palindromeAToZ = palindromeArrayOnlyAToZ.join("");
+  let reversePalindromeAtoZ = reversePalindromeArrayOnlyAToZ.join("");
 
+  if (palindromeAToZ === reversePalindromeAtoZ) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line

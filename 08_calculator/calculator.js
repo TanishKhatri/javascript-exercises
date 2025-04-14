51d8194a7776277f5ce6b9fdaa83b85943cfa660
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-14 08:13:43 +0000

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let sum = 0;
+  for(number of numberArray) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+ let multiply = 1;
+ for(number of numberArray) {
+  multiply *= number;
+ }
+ return multiply;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	numberFactorial = 1;
+  for(let i = number; i > 0; i--) {
+    numberFactorial *= i;
+  }
+  return numberFactorial;
 };
 
 // Do not edit below this line

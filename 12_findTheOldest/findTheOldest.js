1ce2a5102ca611dff2eb200ea8a9e133c6d05afd
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-15 05:00:23 +0000

    Completed findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  
+  let oldestPersonAge = 0;
+  let oldestPerson;
+  let age;
+  for (person of people) {
+    if (person.yearOfDeath == undefined) {
+      const date = new Date();
+      age = date.getFullYear() - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if ((age) > oldestPersonAge) {
+      oldestPersonAge = age;
+      oldestPerson = person;
+    } 
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
